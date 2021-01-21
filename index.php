f7798bacb14b7f1fb48622f4c488807ba2c3374a
maxbehzadi82 <69126271+maxbehzadi82@users.noreply.github.com>
2021-01-21 17:57:58 +0100

    index completed

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+<link rel="stylesheet" href="/css/bootstrap.min.css">
+<link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
     <form action="welcome.php" method="post" class="form-group" enctype="multiple/dataform">
