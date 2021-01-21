259022354f8c516715eabe0d140a69538da89df8
maxbehzadi82 <69126271+maxbehzadi82@users.noreply.github.com>
2021-01-21 18:02:26 +0100

    Update index.php
    index page completed

--- index.php
+++ index.php
@@ -3,25 +3,29 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-<link rel="stylesheet" href="/css/bootstrap.min.css">
-<link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
+
 <body>
     <form action="welcome.php" method="post" class="form-group" enctype="multiple/dataform">
-<div class="row">
-    <input type="text" class="form-control" name="name">
-</div>
-<div class="row">
-    <input type="text" class="form-control" name="name">
-</div>
-<div class="row">
-    <input type="file" class="form-control" name="name">
-</div>
-    <button class="btn">Send<button>
-</form>
+        <div class="row">
+            <input type="text" class="form-control" name="name">
+        </div>
+        <div class="row">
+            <input type="text" class="form-control" name="name">
+        </div>
+        <div class="row">
+            <input type="file" class="form-control" name="name">
+        </div>
+
+        <button class="btn">Send<button>
+    </form>
 </body>
+
 </html>
