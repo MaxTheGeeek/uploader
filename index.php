f5ef89bc3cb9402783c454c0f7451cb28c3f4971
maxbehzadi82 <69126271+maxbehzadi82@users.noreply.github.com>
2021-01-21 17:56:11 +0100

    fields are set

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="welcome.php" method="post" class="form-group" enctype="multiple/dataform">
+<div class="row">
+    <input type="text" class="form-control" name="name">
+</div>
+<div class="row">
+    <input type="text" class="form-control" name="name">
+</div>
+<div class="row">
+    <input type="file" class="form-control" name="name">
+</div>
+    <button class="btn btn-primary"><button>
+</form>
+</body>
+</html>
